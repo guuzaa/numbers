4695cd13fe335684e3ddf4b0874cd593b7fabad9
guza <guuzaa@163.com>
2024-03-24 11:16:07 +0800

    add conversion example

--- examples/conversion.cc
+++ examples/conversion.cc
@@ -2,9 +2,18 @@
 #include "numbers.h"
 
 int main(int argc, char const *argv[]) {
+  std::cout << "==== conversion example ==== \n";
   numbers::u128 a = 35.6;
   numbers::i128 b = 35.6;
-  numbers::i64 c = 35.6;
+  numbers::i64 c = 135.6;
+  std::cout << "a = " << a << ", b = " << b << ", c = " << c << '\n';
+  // do some conversions
+  a = static_cast<numbers::u128>(c);
+  b = static_cast<numbers::i128>(c);
+  std::cout << "a = " << a << ", b = " << b << ", c = " << c << '\n';
+
+  b = 345;
+  c = static_cast<numbers::i64>(b);
   std::cout << "a = " << a << ", b = " << b << ", c = " << c << '\n';
   return 0;
 }
